651308fa635552a13299ebf8fe386c85d8bed3b6
ERFAN <erfan.shokr300@gmail.com>
2022-02-27 19:03:32 +0330

    fixed navbar in base.html

--- products/models.py
+++ products/models.py
@@ -14,6 +14,7 @@ class Slider(models.Model):
 class Navbar(models.Model):
     objects = None
     title = models.CharField(max_length=100)
+    address = models.CharField(max_length=2083)
 
 class Footer(models.Model):
     objects = None
--- products/views.py
+++ products/views.py
@@ -1,12 +1,13 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Product,Slider,Footer
+from .models import Product,Slider,Footer,Navbar
 
 def Car(request):
     products = Product.objects.all()
     sliders = Slider.objects.all()
     footers = Footer.objects.all()
-    return render(request,'index.html',{'products':products,'sliders':sliders,'footers':footers})
+    navbars = Navbar.objects.all()
+    return render(request,'index.html',{'products':products,'sliders':sliders,'footers':footers,'navbars':navbars})
 
 
 def erfan(request):
